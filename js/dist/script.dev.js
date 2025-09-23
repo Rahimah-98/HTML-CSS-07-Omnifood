dc59f323dd2a25fed393fa13fff62f21ec44ee7b
Rahimah Ansari <rahimaansari98@gmail.com>
2025-09-23 14:57:45 +0430

    Implemented Smooth Scrolling

--- js/dist/script.dev.js
+++ js/dist/script.dev.js
@@ -3,12 +3,39 @@
 // Set current year
 var yearEl = document.querySelector('.year');
 var currentYear = new Date().getFullYear();
-yearEl.textContent = currentYear; // Make mobile navigation work
+yearEl.textContent = currentYear; ///////////////////////////////////////////////////////////
+// Make mobile navigation work
 
 var btnNavEl = document.querySelector('.btn-mobile-nav');
 var headerEl = document.querySelector('.header');
 btnNavEl.addEventListener('click', function () {
   headerEl.classList.toggle('nav-open');
+}); ///////////////////////////////////////////////////////////
+// Smooth scrolling animation
+
+var allLinks = document.querySelectorAll('a:link');
+allLinks.forEach(function (link) {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    var href = link.getAttribute('href'); // Scroll back to top
+
+    if (href === '#') window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+
+    if (href !== '#' && href.startsWith('#')) {
+      var sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({
+        behavior: 'smooth'
+      });
+    } // Close mobile navigation
+
+
+    if (link.classList.contains('main-nav-link')) {
+      headerEl.classList.toggle('nav-open');
+    }
+  });
 }); ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 
