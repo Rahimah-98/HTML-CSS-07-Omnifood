9a9cfc7a5674ea40e7d09bb4942095966db3bc1f
Rahimah Ansari <rahimaansari98@gmail.com>
2025-09-24 18:45:21 +0430

    Implemented a sticky navigation bar

--- js/dist/script.dev.js
+++ js/dist/script.dev.js
@@ -11,6 +11,25 @@ var headerEl = document.querySelector('.header');
 btnNavEl.addEventListener('click', function () {
   headerEl.classList.toggle('nav-open');
 }); ///////////////////////////////////////////////////////////
+// Sticky navigation
+
+var sectionHeroEl = document.querySelector('.section-hero');
+var obs = new IntersectionObserver(function (entries) {
+  var ent = entries[0];
+  console.log(ent);
+
+  if (ent.isIntersecting === false) {
+    document.body.classList.add('sticky');
+  } else {
+    document.body.classList.remove('sticky');
+  }
+}, {
+  // In the viewport
+  root: null,
+  threshold: 0,
+  rootMargin: '-80px'
+});
+obs.observe(sectionHeroEl); ///////////////////////////////////////////////////////////
 // Smooth scrolling animation
 
 var allLinks = document.querySelectorAll('a:link');
--- js/script.js
+++ js/script.js
@@ -14,6 +14,31 @@ btnNavEl.addEventListener('click', function () {
 });
 
 ///////////////////////////////////////////////////////////
+// Sticky navigation
+
+const sectionHeroEl = document.querySelector('.section-hero');
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add('sticky');
+    } else {
+      document.body.classList.remove('sticky');
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: '-80px',
+  }
+);
+obs.observe(sectionHeroEl);
+
+///////////////////////////////////////////////////////////
 // Smooth scrolling animation
 const allLinks = document.querySelectorAll('a:link');
 
